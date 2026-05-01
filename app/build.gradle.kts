767298e97baaecc5cca38f84a5089dde73f284aa
Daniel Gutierrez <d.xander.gutierrez@gmail.com>
2026-05-01 12:55:48 -0600

    KAN-41: implementación inicial login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
+
 
     implementation(libs.hilt.android)
     add("ksp", libs.hilt.compiler)
@@ -63,4 +65,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
---
+++ app/src/main/java/com/ecohabits/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.ecohabits.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenPrimary = Color(0xFF1B771B)
+val GreenSecondary = Color(0xFF4CAF50)
+val BackgroundLight = Color(0xFFF5F5F5)
+val BackgroundDark = Color(0xFF212121)
+val White = Color(0xFFFFFFFF)
