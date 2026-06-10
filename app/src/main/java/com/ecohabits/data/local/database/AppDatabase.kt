60a5ee562a46a835172a0fb3585d527202175489
smejiaa19 <silviomejiagarcia@gmail.com>
2026-06-10 12:21:23 -0600

    Agregando cambios en la base de datos, implementando un singleton(Companio Object) context y volatile para seguridad y cohesion entre threads

--- app/src/main/java/com/ecohabits/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/ecohabits/data/local/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.ecohabits.data.local.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.ecohabits.data.local.WeatherChallengeEntity
 import com.ecohabits.data.local.dao.WeatherChallengeDAO
@@ -8,4 +10,21 @@ import com.ecohabits.data.local.dao.WeatherChallengeDAO
 @Database(entities = [WeatherChallengeEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun weatherChallengeDAO() : WeatherChallengeDAO
+    // Se usa companion object ya que es una especie de singleton y esto nos permite garantizar que solo se cree una instancia de la base de datos
+    companion object {
+        @Volatile // Volatile garantiza la seguridad de los subprocesos de la instancia ademas de que permite exponer cambios a otros subprocesos
+        private var INSTANCE: AppDatabase? = null
+        fun getDataBase(context: Context) : AppDatabase{
+            // Importante pasar el context como parametro porque mas adelante lo usaremos para obtener el contexto de la aplicacion
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "App_Data_Base").build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
 }
