85fcc29e8965c07cdac72d2d152bc27d6a7b0f70
smejiaa19 <silviomejiagarcia@gmail.com>
2026-06-17 12:34:36 -0600

    Adding the class that will extend the location service from google

--- app/src/main/java/com/ecohabits/services/LocationForegroundService.kt
+++ app/src/main/java/com/ecohabits/services/LocationForegroundService.kt
@@ -1,5 +1,35 @@
 package com.ecohabits.services
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 
-class LocationForegroundService {
 
+
+class LocationForegroundService : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun requestPermissions() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        )
+            { permissions ->
+                when{
+                    permissions.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                        // Permiso para la location precisa
+                    }
+                    permissions.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                        // Permiso para la location aproximada
+                    }
+                    else -> {
+                        // No hay permiso
+                    }
+                }
+            }
+        locationPermissionRequest.launch(
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
 }
